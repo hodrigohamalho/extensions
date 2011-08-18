1e9db8e5f1523e85b837d50079db02b5463f2c59
Rodrigo Ramalho da Silva <hodrigohamalho@gmail.com>
2011-08-18 01:22:53 -0300

    flash slide show

---
+++ eXo WCM/javascripts/flash-slide-show/js/index.js
@@ -0,0 +1,31 @@
+jQuery(function(){
+	// Cria uma pilha para permitir que seja possivel uma pausa entre a exibicao dos flahs
+	var stack = {
+		delay: 5000,
+		actions:[],
+		run: function() {
+			if (stack.actions.length) { // Verifica se existe elemento na pilha
+				stack.actions.shift()(); // Desempilha
+		    	setTimeout(stack.run, stack.delay); // Semelhante ao sleep do java.
+		  	}else{ // Quando a pilha ficar vazia recomece o ciclo.
+		  		$($("#slideshow li")).hide();
+			  	showElements();
+			  	stack.run();
+			}
+		  	
+		}
+	};
+
+	function showElements(){
+		$("#slideshow li").each(function(index){ // itera sobre os itens
+			var that = this;
+			stack.actions.push(function(){ // empilha essas acoes
+				$(that).show();	// Mostra o flash atual
+				$($("#slideshow li")[index-1]).hide("slow"); // Esconde o flash anterior
+			});
+		});
+	}
+
+	showElements();
+	stack.run();
+});

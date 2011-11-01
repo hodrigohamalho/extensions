1c2e2a3d824779d2556649c716561b93fd56d1a5
Rodrigo Ramalho da Silva <hodrigohamalho@gmail.com>
2011-10-31 22:06:36 -0200

    adjusting..

--- change_gatein_portalcontainer.sh
+++ change_gatein_portalcontainer.sh
@@ -1,56 +1,67 @@
 #!/bin/sh
-
-#arg 0: new container name.
-#arg 1: ear name of eppsp.ear (unexploded)
-
-if [ $# -lt 1 ] ; then
-  echo "Wrong number of parameters."
-  echo "Usage:"
-  echo "./rename_all newContainerName \n"
-  exit 1
-fi
-
-echo "Explodind files..."
+############################################################
+#
+# Simple script to rename the portal container 'ecmdemo' to
+# the new name passed through parameter.
+#
+# After usage this script remember to rename the <jndi-name>
+# in file wcm-ds.xml to gatein-idm_newname and gatein-jcr_newname
+#
+# arg 0: new container name.
+# arg 1: ear name of eppsp.ear (unexploded)
+#
+# @author Rodrigo Ramalho (hodrigohamalho@gmail.com)
+# @version 0.1
+#
+############################################################
 
 NEW_CONTAINER_NAME=$1
 EAR_NAME=$2
 
-echo "Exploding $EAR_NAME"
-unzip $EAR_NAME -d $EAR_NAME.'exploded' &&
-rm $EAR_NAME &&
-mv $EAR_NAME.'exploded' $EAR_NAME &&
+function explodeFiles(){
+  FILE_TO_EXPLODE=$1
+	echo "Exploding: $FILE_TO_EXPLODE ... "
+	
+  unzip -q $FILE_TO_EXPLODE -d $FILE_TO_EXPLODE.'exploded'
+  rm $FILE_TO_EXPLODE
+  mv $FILE_TO_EXPLODE.'exploded' $FILE_TO_EXPLODE 
+}
 
-echo "Exploding $EAR_NAME/ecmdemo.war" &&
-unzip $EAR_NAME/ecmdemo.war -d $EAR_NAME/ecmdemo.war.exploded &&
-rm $EAR_NAME/ecmdemo.war &&
-mv $EAR_NAME/ecmdemo.war.exploded $EAR_NAME/ecmdemo.war &&
+function replaceTextReferences(){
+	echo "Replacing ecmdemo references to $NEW_CONTAINER_NAME ... "
+	
+  FILES=$(find $EAR_NAME -name "*ecmdemo*")
+  find $EAR_NAME -type f -exec sed -i "" -e s/ecmdemo/"$NEW_CONTAINER_NAME"/g {} \;
+}
 
-echo "Exploding $EAR_NAME/rest-ecmdemo.war" &&
-unzip $EAR_NAME/rest-ecmdemo.war -d $EAR_NAME/rest-ecmdemo.war.exploded &&
-rm $EAR_NAME/rest-ecmdemo.war &&
-mv $EAR_NAME/rest-ecmdemo.war.exploded $EAR_NAME/rest-ecmdemo.war &&
+function renameFilesAndDirs(){
+  echo "Renaming files and dirs... "
 
-echo "Exploding $EAR_NAME/lib/exo-ecms-packaging-ecmdemo-config-2.1.5-CP02.jar" &&
-unzip $EAR_NAME/lib/exo-ecms-packaging-ecmdemo-config-2.1.5-CP02.jar -d $EAR_NAME/lib/exo-ecms-packaging-ecmdemo-config-2.1.5-CP02.jar.exploded &&
-rm $EAR_NAME/lib/exo-ecms-packaging-ecmdemo-config-2.1.5-CP02.jar &&
-mv $EAR_NAME/lib/exo-ecms-packaging-ecmdemo-config-2.1.5-CP02.jar.exploded $EAR_NAME/lib/exo-ecms-packaging-ecmdemo-config-2.1.5-CP02.jar &&
+  FILES=$(find $EAR_NAME -name "*ecmdemo*")
+  for FILE in $FILES; do
+    NAME="$FILE"
+    NAME_REPLACED=$(echo $FILE | sed -e s/ecmdemo/"$NEW_CONTAINER_NAME"/g)
+    mv $NAME $NAME_REPLACED
+  done
+}
 
-FILES_PATH="."
-FILES=$(find $FILES_PATH -name "*ecmdemo*")
+if [ $# -lt 1 ] ; then
+  echo "Wrong number of parameters."
+  echo "Usage:"
+  echo "./change_gatein_portalcontainer.sh newContainerName gatein-eppspdemo-portal-2.1.5-CP02.ear \n"
+  exit 1
+fi
 
-echo "Substituindo todas referencias de ecmdemo para $NEW_CONTAINER_NAME"
+#############################################################
+#
+# Script Begin
+#
+#############################################################
 
-find . -type f -exec sed -i "" -e s/ecmdemo/"$NEW_CONTAINER_NAME"/g {} \;
+explodeFiles $EAR_NAME &&
+explodeFiles $EAR_NAME/ecmdemo.war &&
+explodeFiles $EAR_NAME/rest-ecmdemo.war &&
+explodeFiles $EAR_NAME/lib/exo-ecms-packaging-ecmdemo-config-2.1.5-CP02.jar &&
 
-echo "Referencias atualizadas."
-
-echo "Renomeando arquivos e diretorios..."
-
-for FILE in $FILES; do
-  NAME="$FILE"
-  NAME_REPLACED=$(echo $FILE | sed -e s/ecmdemo/"$NEW_CONTAINER_NAME"/g)
-  mv $NAME $NAME_REPLACED
-done
-
-echo "Arquivos e diretorios atualizados."
-
+replaceTextReferences &&
+renameFilesAndDirs 
